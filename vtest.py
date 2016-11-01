1e190bd86417743ff6d622639a50b69255d9bc4d
Nilay Thakor <nthakor@nd.edu>
2016-11-01 23:36:39 +0530

    videoReadandWrite

---
+++ vtest.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+ 
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    sift = cv2.SIFT()
+    kp = sift.detect(gray,None)
+    img=cv2.drawKeypoints(gray,kp)
+    cv2.imshow('frame',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
