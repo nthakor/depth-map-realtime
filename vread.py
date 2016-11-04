c9e43216ebebafe270781ea4d9b6dc7a4f12613a
Nilay Thakor <nthakor@nd.edu>
2016-11-04 22:08:39 +0530

    realtime key point display

--- vread.py
+++ vread.py
@@ -6,8 +6,10 @@ cap = cv2.VideoCapture('vtest.avi')
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp, des = sift.detectAndCompute(gray,None)
+    frame=cv2.drawKeypoints(gray,kp,frame)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
